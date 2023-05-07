f745295847edff2a07f19149b4884ccf80be9dec
hao0516hsu <chhsu0516@gmail.com>
2023-05-07 17:20:50 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: "main" }))
+app.set('view engine', 'handlebars')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`)
+})
