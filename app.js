77385487d1175dc674172d98635d24e098c574e0
hao0516hsu <chhsu0516@gmail.com>
2023-05-09 17:47:06 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-const Url = require('./models/url')
 const app = express()
 const port = 3000
 
 const routes = require('./routes/index')
+require('./config/mongoose')
+
 app.engine('handlebars', exphbs({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
-// mongoose連線設定
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // 連線狀態
 app.listen(port, () => {
